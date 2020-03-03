323129c8bb28a34e3d5061eca3332fafe7951a3b
Terwase Gberikon <7498753+tekrondo@users.noreply.github.com>
2020-03-03 09:28:22 +0100

    Added a CORS policy to make the server request from the frontend to the backend uninhibited

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const { mongoose } = require('./db/mongoose')
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const { List, Task } = require('./db/models')
 
 app.get('/lists', (req, res) => {
