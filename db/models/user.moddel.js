a3c0c5c6be101a514c3cae38209ac177a39417da
Terwase Gberikon <7498753+tekrondo@users.noreply.github.com>
2020-05-27 16:42:30 +0100

    Hashed and salted the password to create a JWT for user authentication

--- db/models/user.moddel.js
+++ db/models/user.moddel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto')
+const bcrypt = require('bcryptjs');
 
 const jwtSecret = "_8Txggu7A3v1csMRlwRPtsy7Izpff2RDDWCWTBR6aTE";
 
@@ -78,6 +79,60 @@ UserSchema.methods.createSession = function() {
   })
 }
 
+
+/* STATIC MODEL METHODS */
+UserSchema.statics.findByIdAndToken = function(_id, token) {
+  const User = this;
+
+  return User.findOne({
+    _id,
+    "sessions.token": token
+  })
+}
+
+UserSchema.statics.findByCredentials = function(password, email) {
+  let User = this;
+  return User.findOne({ email }).then((user) => {
+    if(!user) return Promise.reject()
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res) resolve(user);
+        else{
+          reject()
+        }
+      })
+    })
+  })
+}
+
+UserSchema.statics.hasRefreshTokenExpired = (expiresAt) => {
+  let secondsSinceEpoch = Date.now / 1000;
+  if(expiresAt > secondsSinceEpoch ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+UserSchema.pre('save', function(next) {
+  let user = this;
+  let costFactor = 10;
+
+  if(user.isModified('password')) {
+
+    bcrypt.genSalt(costFactor, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+  
+})
+
 let saveSessionToDatabase = (user, refreshToken) => {
   return new Promise((resolve, reject) => {
     let expiresAt = generateRefreshTokenExpiryTime();
@@ -97,3 +152,6 @@ let generateRefreshTokenExpiryTime = () => {
   let secondsUntilExpire = ((daysUntilExpire * 24) * 60) * 60;
   return ((Date.now() / 1000 ) + secondsUntilExpire )
 }
+
+const User = mongoose.model("User", UserSchema)
+module.exports = { User }
